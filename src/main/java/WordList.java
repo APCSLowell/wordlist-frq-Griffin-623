997820d58d78f7876a58650f25b38b8620a59e14
Griffin-623 <79292572+Griffin-623@users.noreply.github.com>
2024-04-03 12:11:15 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -12,7 +12,7 @@ public class WordList
   {
     int count = 0;
     for(int i =0;i<myList.size();i++){
-      if(myList.get(i).length==len){
+      if(myList.get(i).length()==len){
         count++;
       }
     }
@@ -23,7 +23,7 @@ public class WordList
   public void removeWordsOfLength(int len)
   {
     for(int i=0;i<myList.size();i++){
-      if(myList.get(i).length==len){
+      if(myList.get(i).length()==len){
         myList.remove(i);
         i--;
       }
